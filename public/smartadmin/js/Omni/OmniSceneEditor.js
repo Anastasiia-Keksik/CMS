0df037f006728b869d71267434882d6420ec2c2e
PaYo90 <skorpss@gmail.com>
2021-02-02 22:19:24 +0100

    02.02.2021

--- public/smartadmin/js/Omni/OmniSceneEditor.js
+++ public/smartadmin/js/Omni/OmniSceneEditor.js
@@ -5,12 +5,52 @@ function newImage(image, id){
         img.nameid = id
         canvas.add(img)
         img.on('modified', function (){
+            if (typeof img.angle != 'undefined'){
+                img.angle = Math.round(img.angle)
+            }
+
+            console.log("Image: ")
             console.log(img)
+            console.log("Layers: ")
+            console.log(layers)
+            let it0 = 0;
+            layers.forEach(function (layer) {
+                if(layer.id === img.nameid){
+                    layers[it0].width = img.width * img.scaleX
+                    layers[it0].height = img.height * img.scaleY
+                    layers[it0].posx = img.left
+                    layers[it0].posy = img.top
+                    layers[it0].rot = img.angle
+                    console.log(layers)
+                }
+                it0++
+            })
+        }).on('selected', function () {
+            let it0 = 0;
+            layers.forEach(function (layer) {
+                if (layer.id === img.nameid){
+                    let tmp_layer = layers[it0]
+                    layers.splice(it0, 1)
+                    layers.push(tmp_layer)
+                    prependLayers()
+                    console.log(layers)
+                }
+                it0++
+            })
         })
     })
 }
 
 $(document).ready(function (){
+    $('#layer-window').sortable({
+        handle: ".handle"
+    }).selectable({
+        filter: "div", cancel: ".handle",
+        selected: function( event, ui ) {
+            console.log(ui)
+        }
+    })
+
     $('.image-drag').draggable({
         helper: "clone",
         appendTo: 'body',
@@ -21,45 +61,23 @@ $(document).ready(function (){
         }
     }).disableSelection()
     $('#shock-wave').droppable({
+        accept: ".draggable",
         drop: function (event, ui){
-            helper.dateid = Date.now()
-            layers.push(helper)
-            console.log(helper.data('imagename'))
+            let tmp_layer = [];
+            tmp_layer.id = Date.now()
+            tmp_layer.name = ''
+            tmp_layer.width = ''
+            tmp_layer.height = ''
+            tmp_layer.posx = ''
+            tmp_layer.posy = ''
+            tmp_layer.rot = ''
+            tmp_layer.opacity = ''
+            tmp_layer.url = helper.data('imagename')
+            tmp_layer.thumbUrl = helper[0].src
+            layers.push(tmp_layer)
+            newImage(tmp_layer.url, tmp_layer.id)
 
-
-            newImage(helper.data('imagename'), helper.dateid)
-
-            $("#layer-window").html('')
-            let it1 = 1;
-            layers.forEach(function (elm) {
-                $("#layer-window").prepend('<li id="'+elm.dateid+'" contenteditable="true">Warstwa '+it1+'</li>')
-
-                $('#'+elm.data('usertoobjconid')).bind("keypress", function(e) {
-                    if($('#'+elm.data('usertoobjconid')).text().length > 64) {
-                        //document.execCommand("undo");
-                        e.preventDefault()
-                    }
-
-                    if (e.keyCode === 13){
-                        e.preventDefault()
-                    }
-                        elm[0].name = $('#'+elm.data('usertoobjconid')).text()
-                }).bind('paste', function (event){
-                        if($('#'+elm.data('usertoobjconid')).text().length > 64) {
-                            //document.execCommand("undo");
-                            e.preventDefault()
-                        }
-                        event.preventDefault()
-
-                    let paste = event.originalEvent.clipboardData.getData('text/plain').replace('/(?:\r\n|\r|\n)/g', ' ')
-                    $('#'+elm.data('usertoobjconid')).text($('#'+elm.data('usertoobjconid')).text()+paste)
-
-                })
-                if(elm[0].name !== ""){
-                    $('#'+elm.data('usertoobjconid')).text(elm[0].name)
-                }
-                it1++
-            })
+            prependLayers()
         }
     })
 
@@ -97,6 +115,42 @@ $(document).ready(function (){
     })
 })
 
+function prependLayers(){
+    $("#layer-window").html('')
+    let it1 = 1;
+    layers.forEach(function (elm) {
+        $("#layer-window").prepend('<div id="'+elm.id+'" class="ui-widget-content wers d-flex position-relative" style="padding: 10px">' +
+            '<div class="handle" style="position: absolute; top: 0; left: 0; height: 42px; width: 42px; margin-left: 0; margin-right: 8px; background-image: url(\''+elm.thumbUrl+'\'); background-repeat: no-repeat; background-size: cover;"></div>'+
+            '<span id="edit_'+elm.id+'"  style="min-width: 60px; color: black; margin-left: 48px" contenteditable="true" data-toggle="tooltip" data-placement="auto" title="Right click to name change">Warstwa '+it1+'</span>' +
+            '</div>')
+
+        $('#'+elm.id).bind("keypress", function(e) {
+            if($('#'+elm.id).text().length > 64) {
+                //document.execCommand("undo");
+                e.preventDefault()
+            }
+
+            if (e.keyCode === 13){
+                e.preventDefault()
+            }
+            elm.name = $('#edit_'+elm.id).text()
+        }).bind('paste', function (event){
+            if($('#edit_'+elm.id).text().length > 64) {
+                //document.execCommand("undo");
+                e.preventDefault()
+            }
+            event.preventDefault()
+
+            let paste = event.originalEvent.clipboardData.getData('text/plain').replace('/(?:\r\n|\r|\n)/g', ' ')
+            $('#edit_'+elm.id).text($('#edit_'+elm.id).text()+paste)
+
+        })
+        if(elm.name !== ""){
+            $('#edit_'+elm.id).text(elm.name)
+        }
+        it1++
+    })
+}
 
 function zoom(zoom){
     let zoomState = $('#shock-wrapper')
